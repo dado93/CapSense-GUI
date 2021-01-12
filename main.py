bb8ec2d18c834550550e079e339730dddd585a43
Davide Marzorati <davide4.marzorati@mail.polimi.it>
2021-01-12 17:36:37 +0100

    Implemented main thread for data reading. Computation of sample rate added

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from kivy.app import App
 from kivy.clock import Clock
-from kivy.config import Config
-Config.set('kivy', 'exit_on_escape', '0')
-from kivy.uix.boxlayout import BoxLayout
+from kivy.core.window import Window
 from kivy.lang import Builder
 from kivy.properties import ObjectProperty
+from kivy.uix.boxlayout import BoxLayout
+
+import communication
 from communication import MIPSerial
+
+from kivy.config import Config
+Config.set('kivy', 'exit_on_escape', '0')
+Config.set('kivy', 'desktop', '1')
 
 Builder.load_file('toolbar.kv')
 Builder.load_file('bottom_bar.kv')
@@ -15,17 +20,25 @@ Builder.load_file('graph_tabs.kv')
 
 class ContainerLayout(BoxLayout):
     
+    top_bar = ObjectProperty(None)
     toolbar = ObjectProperty(None)
     bottom_bar = ObjectProperty(None)
     progress_bar = ObjectProperty(None)
+    graph_tabs = ObjectProperty(None)
     
     def __init__(self, **kwargs):
         self.serial = MIPSerial()
+        self.serial.bind(connected=self.connection_event)
         super(ContainerLayout, self).__init__(**kwargs)
-        # Start moving progress bar
+        Window.bind(on_request_close=self.exit_check)
+        # Start moving progress bar based on connection status
         self.pb_update_sign = 1
         self.pb_update_event = Clock.schedule_interval(self.progress_bar_update, 0.05)
     
+    def exit_check(self, *args):
+        print('Exit check')
+        return False
+
     def on_toolbar(self, instance, value):
         try:
             self.toolbar.bind(message_string=self.bottom_bar.update_text)
@@ -38,12 +51,52 @@ class ContainerLayout(BoxLayout):
             self.serial.bind(message_string=self.bottom_bar.update_text)
         except:
             raise
+
+    def on_top_bar(self, instance, value):
+        """!
+        @brief Callback called when the top bar widget is ready.
+
+        This function is called when the top bar widget is ready
+        to be displayed on the screen. Here we bind the battery
+        voltage property of the serial object to a callback 
+        of the top bar widget, so that the battery voltage is
+        updated properly.
+        """
+        self.serial.bind(battery_voltage=self.top_bar.update_battery_level)
     
-    def progress_bar_update(self, dt):
-        if (self.serial.connected == 2):
+    
+    def on_graph_tabs(self, instance, value):
+        self.serial.bind(data_sample_rate=self.graph_tabs.setter('data_sample_rate'))
+        self.serial.bind(temperature_sample_rate=self.graph_tabs.setter('temperature_sample_rate'))
+
+    def connection_event(self, instance, value):
+        """!
+        @brief Callback called on serial connection event.
+
+        This callback is called when there is a connection event
+        from the serial port. It is called when the board is
+        found on one serial port, when the board is connected
+        and when the board is disconnected.
+        @param value the current connection status
+        @param instance the instance calling this callback
+        """
+        if (value == communication.BOARD_CONNECTED):
             self.progress_bar.value = 100
             self.pb_update_event.cancel()
+            self.top_bar.enable_widgets(True)
+            self.toolbar.disabled = False
+            self.pb_update_sign = 0
+        elif (value == communication.BOARD_DISCONNECTED):
+            self.top_bar.enable_widgets(False)
+            self.toolbar.disabled = True
+            if (self.pb_update_sign == 0):
+                self.progress_bar.value = 0
+                self.pb_update_sign = 1
+                self.pb_update_event = Clock.schedule_interval(self.progress_bar_update, 0.05)
 
+    
+
+    def progress_bar_update(self, dt):
         if (self.pb_update_sign > 0):
             self.progress_bar.value += 1
             if (self.progress_bar.value == 100):
--- top_bar.py
+++ top_bar.py
@@ -1,8 +1,15 @@
 from communication import MIPSerial
-from kivy.properties import ObjectProperty
+from kivy.properties import ObjectProperty, BooleanProperty
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle
 
 class TopBar(BoxLayout):
+    enable_buttons = BooleanProperty(False)
+    start_streaming_button = ObjectProperty(None)
+    stop_streaming_button = ObjectProperty(None)
+    battery_label = ObjectProperty(None)
+
     def __init__(self, **kwargs):
         super(TopBar, self).__init__(**kwargs)
         self.ser = MIPSerial()
@@ -12,3 +19,35 @@ class TopBar(BoxLayout):
 
     def stop_streaming(self):
         self.ser.stop_streaming()
+
+    def enable_widgets(self, value):
+        """!
+        @brief Enable/disable widgets for interaction with board.
+        """
+        self.start_streaming_button.disabled = (not value)
+        self.stop_streaming_button.disabled = (not value)
+        self.battery_label.disabled = (not value)
+    
+    def update_battery_level(self, instance, value):
+        self.battery_label.text = f'Battery: {value:.2f}'
+        if (value >= 3.7):
+            self.battery_label.update_color(0,1,0)
+            self.battery_label.color = (0,0,0,1)
+        elif (value >= 3.3 and value < 3.7):
+            self.battery_label.update_color(1,1,0)
+            self.battery_label.color = (0,0,0,1)
+        else:
+            self.battery_label.update_color(1,0,0)
+            self.battery_label.color = (1,1,1,1)
+
+class ColoredLabel(Label):
+    def update_color(self, r, g, b):
+        self.canvas.before.clear()
+        with self.canvas.before:
+            Color(r,g,b,1)
+            self.rect = Rectangle(pos=self.pos, size=self.size)
+        self.bind(pos=self.update_rect,
+                    size=self.update_rect)
+    def update_rect(self,*args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
